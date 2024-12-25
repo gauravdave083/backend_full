f90ef1cb224b2c82ab4acddbfff3764714e30413
Gaurav Vijay Dave <122374158+gauravdave083@users.noreply.github.com>
2024-12-25 08:25:44 +0000

    updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require("http");
-const fs = require("fs")
+const fs = require("fs");
+const url = require("url");
 
 // const myServer = http.createServer((req, res) => {
 //      console.log("New Request Recieved")
@@ -7,15 +8,35 @@ const fs = require("fs")
 // });
 
 const myServer = http.createServer((req, res) => {
+     if (req.url === "/favicon.ico") return res.end
      const log = `${Date.now()}: ${req.url} New Req Recieved \n`
+     const myUrl = url.parse(req.url, true);
+
+
      fs.appendFile("log.txt", log , (err, data) => {
-          switch (req.url) {
+          switch (myUrl.pathname) {
                case "/":
-                    res.end("Homepage")
+                    if (req.method === "GET") res.end("Homepage")
                     break;
+
                case "/about":
-                    res.end("I am Gaurav Dave")
+                    const username = myUrl.query.myname;
+                    res.end(`Hi, ${username}`)
                     break;
+
+               case "/search":
+                    const search = myUrl.query.search_query;
+                    res.end("Here are your results for" + search);
+                    break;
+               
+               case "/signup":
+                    if (req.method === "GET") res.end("This is your signup form");
+                    else if (req.method === "POST") {
+                         // DB Query
+                         res.end("Success");
+                    }
+                    break;
+
                default:
                     res.end("404 Not Found");
           }
